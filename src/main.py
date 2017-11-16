4c265e0e93008f1bc1b54411dafe4093eb12244e
Zeeshan Hyder <hyder.zeeshan@live.com>
2017-11-16 15:44:04 -0500

    updated main file

--- src/main.py
+++ src/main.py
@@ -2,10 +2,8 @@ import numpy as np
 import pandas as pd
 from sklearn import linear_model
 from sklearn.svm import LinearSVC
-import csv
 
 
-TEST_PORTION=.5
 test_data_src = "/home/hyder/ds/porto/test.csv"
 train_data_src = "/home/hyder/ds/porto/train.csv"
 
@@ -26,29 +24,17 @@ num_cols= list(set(list(full_data.columns))-set(id_col)-set(target_col))
 other_col = ['Type']
 
 
-for var in num_cols:
-    if full_data[var].isnull().any()==True:
-        full_data[var+'_NA']=full_data[var].isnull()*1
-
-full_data[num_cols] = full_data[num_cols].fillna(full_data[num_cols].mean(),inplace=True)
-
-
 
 train=full_data[full_data['Type']=='Train']
 test=full_data[full_data['Type']=='Test']
 
 
 
-train['is_train'] = np.random.uniform(0, 1, len(train)) <= .75
-Train, Validate = train[train['is_train']==True], train[train['is_train']==False]
-
 features=list(set(list(full_data.columns))-set(id_col)-set(target_col)-set(other_col))
 
-x_train = Train[list(features)].values
-y_train = Train['target'].values
+x_train = train[list(features)].values
+y_train = train['target'].values
 
-x_validate = Validate[list(features)].values
-y_validate = Validate['target'].values
 
 x_test = test[list(features)].values
 
