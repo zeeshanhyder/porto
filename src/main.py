9ac4573efde40511ca2b614b862113645748f2e8
Zeeshan Hyder <hyder.zeeshan@live.com>
2017-11-17 15:51:27 -0500

    cleanup

--- src/main.py
+++ src/main.py
@@ -4,51 +4,36 @@ from sklearn import linear_model
 from sklearn.svm import LinearSVC
 
 
+# dataset size to work on
+SIZE = 200000
 # test.csv and train.csv location
-test_data_src = "/home/hyder/ds/porto/test.csv"
-train_data_src = "/home/hyder/ds/porto/train.csv"
+data_src = "/home/hyder/ds/porto/train.csv"
 
 
 # Load the test and train csv
-#test_data = pd.read_csv(test_data_src)
-train_data = pd.read_csv(train_data_src)
+data = pd.read_csv(data_src)
 
 # To make things easier, let's label our test and train data before merging them
-#test_data['Type'] = 'Test'
 
-# merge the test and train data
-#full_data = pd.concat([train_data,test_data],axis=0)
-
-train_data = train_data.head(200000)
-full_data = train_data
-test_data = train_data.tail(200000)
-train_data['Type'] = 'Train'
-test_data['Type'] = 'Test'
-
-# since the dataset is very large, let's start by working on small part of it [1000 entries from each data set]
-#full_data = pd.concat([full_data.head(100000),full_data.tail(100000)],axis=0)
-
+# divide the data in train and test
+train = data.head(SIZE)
+test = data.tail(SIZE)
+train['Type'] = 'Train'
+test['Type'] = 'Test'
 
 
 # recognise the ID field, and target [target is 0 or 1 i.e whether the driver claims insurance or not]
 id_col = ['id']
 target_col = ['target']
 # total number of columns
-num_cols= list(set(list(full_data.columns))-set(id_col)-set(target_col))
-
+num_cols= list(set(list(data.columns))-set(id_col)-set(target_col))
 # this is our own very recently created column
 other_col = ['Type']
 
-# divide the data in test and train here
-#train=full_data[full_data['Type']=='Train']
-#test=full_data[full_data['Type']=='Test']
-
-train = train_data
-test = test_data
 
 
 # feature is all other columns except target, id and Type column that we created
-features=list(set(list(full_data.columns))-set(id_col)-set(target_col)-set(other_col))
+features=list(set(list(data.columns))-set(id_col)-set(target_col)-set(other_col))
 
 # X and y of our data to train on
 x_train = train[list(features)].values
@@ -69,5 +54,8 @@ clf.fit(x_train, y_train)
 # print the prediction if driver will claim insurance or not
 predict = clf.predict(x_test)
 
+# calculate the accuracy num_of_correct_predictions/total_predictions
 acc=(y_true == predict).sum()/((y_true == predict).sum()+(y_true != predict).sum())
+
+print("Accuracy for",SIZE," entries:")
 print(acc)
