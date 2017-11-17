94469a1042c44c09c24dda70a3099addf0943009
Zeeshan Hyder <hyder.zeeshan@live.com>
2017-11-17 15:38:53 -0500

    - removed test data - introduced scoring from louis code implemented SGDClassifier

--- src/main.py
+++ src/main.py
@@ -10,19 +10,23 @@ train_data_src = "/home/hyder/ds/porto/train.csv"
 
 
 # Load the test and train csv
-test_data = pd.read_csv(test_data_src)
+#test_data = pd.read_csv(test_data_src)
 train_data = pd.read_csv(train_data_src)
 
 # To make things easier, let's label our test and train data before merging them
-test_data['Type'] = 'Test'
-train_data['Type'] = 'Train'
+#test_data['Type'] = 'Test'
 
 # merge the test and train data
-full_data = pd.concat([train_data,test_data],axis=0)
+#full_data = pd.concat([train_data,test_data],axis=0)
 
+train_data = train_data.head(200000)
+full_data = train_data
+test_data = train_data.tail(200000)
+train_data['Type'] = 'Train'
+test_data['Type'] = 'Test'
 
 # since the dataset is very large, let's start by working on small part of it [1000 entries from each data set]
-full_data = pd.concat([full_data.head(1000),full_data.tail(1000)],axis=0)
+#full_data = pd.concat([full_data.head(100000),full_data.tail(100000)],axis=0)
 
 
 
@@ -36,8 +40,11 @@ num_cols= list(set(list(full_data.columns))-set(id_col)-set(target_col))
 other_col = ['Type']
 
 # divide the data in test and train here
-train=full_data[full_data['Type']=='Train']
-test=full_data[full_data['Type']=='Test']
+#train=full_data[full_data['Type']=='Train']
+#test=full_data[full_data['Type']=='Test']
+
+train = train_data
+test = test_data
 
 
 # feature is all other columns except target, id and Type column that we created
@@ -50,12 +57,17 @@ y_train = train['target'].values
 
 # This is what we will use in predict to test our classifier against [test data]
 x_test = test[list(features)].values
+y_true = test['target'].values
+
 
 
 # LinearSVC classifier [Later on we will use SGD]
-clf = LinearSVC(random_state=0)
+clf = linear_model.SGDClassifier(n_jobs=3)
 # fit
 clf.fit(x_train, y_train)
 
 # print the prediction if driver will claim insurance or not
-print( clf.predict(x_test) )
+predict = clf.predict(x_test)
+
+acc=(y_true == predict).sum()/((y_true == predict).sum()+(y_true != predict).sum())
+print(acc)
